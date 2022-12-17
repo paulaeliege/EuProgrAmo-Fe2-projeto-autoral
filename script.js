60cd77d73d10b12ab5f07cc3fcc29dd692054001
Paula <paulaeliegeferreira@gmail.com>
2022-12-17 16:28:31 -0300

    atualizando javascript

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ function carregaProjetoAutoral() {
     var biografias = {
         bio01: {
             nome: "Confeitaria",
-            imagem: "https://lh3.googleusercontent.com/7FWXLxjGvFrPa6bYo5WD9v-T_1axVRVUop3H-a8hd8fyiwQ42yjAk_7WQzTLnOtkjaektQb4uXBq-45NQAKGVFl4y7sCLrdoRUlgkDrAofMk3lJUAYo7G1DQo1gE7e2LYTvaZe7ggNrH2unV2ZXQtMXWwUMApWGDyXhjZGC26geM1zcYTCjJmHDEa-bKRVFHAtCWuWGlLqaTBGs0rhHmEjPRYElqcduhR5EB3-w6eysptfddLYw9DcOP9vJnK9n_Bi8KIindgacqX4kPVtwZthSBBrQLPTnzX8lItGe16stxk41Tnb8KKqX1Y9Cs1jHxL885-EhQfZXpV7tNe2oTndLFx7xswgin3JVe2NOFI-fgkJDHpCprsKyupFX09O6o5yVQkqatxC1iNDRN4S4vi5VM24GGdNLutocEtpkDGOpIQrei_I4cLzC6WBD_Xton3O6-znpmZO5mDC_xzDOpBbWkamM9HB1NHKvcuMaNYYqiYTNewHYC1mFRYK5T7TkL4WOIgmCoSxiHJn16EEGFKZXU-kwIRT_peHr9Gi12giDBwULG6TtMWnZ8EcLbLRPlXVVDnUVG-xq_huNkP462Ctyy659fD7O2fAJ_XxHaPX3lP8db6a3XSn108Maswa2z4DNlFR3NBghlDfTsQVDSXlJfgpHacqf8kZkGKcoOkO13OobVbA7XGpmbi94j-QeENfFW8FOOnSDYPfRpaQBjSwW2AOUVpK3Vpk2qXn2pFqYOYlhez4msYjsmcs08_PwBbcfcSlSCCTpx-PrEmdlJMhCFdVtXSQdy-mPBLQG0_q5EW5ba4nDRV59uS45ei9QFYQbOTXUw8_7jwLxpjaxFKjtzWV8_mLufwkXqRJZXOTD4kHvr-4Jj74OKpWww9oCrtsX5cx3ch9SB1tLIRzObFjVWftwresRpLaeKj62Nuc2xwBVbzw=w720-h718-no?authuser=0",
+            imagem: "https://i.postimg.cc/Pxg1J1gm/Whats-App-Image-2022-12-16-at-15-26-39-2.jpg",
             descricao: "Olá eu sou a Paula, tenho 47 anos,  há 5 anos atrás queria seguir carreira de confeiteira, cheguei até criar alguns pratos e investir neles. A confeitaria me levou a lugares que jamais imaginaria chegar, conheci pessoas e lugares maravilhosos na grande São Paulo.",
             citacao: "A  confeitaria ultrapassa a exatidão das medidas dos ingredientes e as técnicas. É adicionar amor e prazer no que se faz! Seja um bolo simples, com memória afetiva, ou um preparo mais elaborado. Não há distinção! Bruno Rucy Marinho."
         },
 
         bio02: {
             nome: "Surpresa de Morango",
-            imagem: "https://lh3.googleusercontent.com/F-Sx-CqWUD0mNDFULTXHNnCkXlw_-Jm_RPxJF7NDZFXnxcs3Y3Ij3XCKjjH-hhEIC0-Pp2ahE648p188_Zn-BrPbUNdP5f3_ekXUCu_NStsf7rrq1jG6w4CrGb4_8W-V78P4GgMoXrSJGzRs2ZfgWXv7eX4Sis6TpJt_jzrK_QijhqcjG0Vc-sRpvYn5v_lc-bzLCgWCrLxxnc_vQrMUTBiHDyDntCrW9UbUugB15U_oVdEqW6pqFmrXUn2o9PXQo1qCf9wEvz4sdLnnKqPqo3mcevrUdLEvyrCUXfv2aC4Vont0oGBtBAeiUK87HKEXVTgFv_OdIa40QRv9q0QpBqg9WoQ3Nxydtca-MznKdTl1lAdiATVq59ozuEIp8i0jugW9pFRVuznLK-pEf7PSdaZEMWW1SK5QndhdPWgUCFZG3NJu5yn0nZGkTEpe9uuf66tS-q9m-cYEPmbuwCrG8YlWCou8HmJK1UaGnwMEIMesOzzjuxMef_QGTS-aSJy8wd45Yz0rGk_CybSgudvQ0_cjgI1zaLSNTlY7_UgANU-VgCVwjJ-Rg56OpMc7sKwJcFCW-BI31j7DCyo-q4aGPiia0hNEtMgKg3r4rVoYQ6nqlENl01ms6WTLxy_QUmxqCUDoJEvyvKBAv1AM6Q2eKC_efEQfDEEPWMMvQrVc3wk2pPEDJc5XBwIqfROMYb2pNzqc9DFkITKAkzy90ot1PKN8SAU_lKtemZhHMKsamzpAueGd2xPZHbC0fVuTNdsZ50xmNYmNs0D8qkTsfnSlkK7kMzPyveza8nxFaIH31IqfcLsWMexi_X5rbvcWfDUSKl4sF2CkepkmpoBH_ZMPjgaqbVBxHRQviv9WAsywG34G6VHsw89bw9-LeHnYGNhbWRjVrWSpRmQtGHyN74axZcptl1ZlYKQZwZGp2o6feMT9WkJ5Gw=w720-h708-no?authuser=0",
+            imagem: "https://i.postimg.cc/G3DkNSDz/Whats-App-Image-2022-12-16-at-16-18-23.jpg",
             descricao: "Surpresa de Morango, um doce que remete amor e carinho o sabor do chocolate junto com o morango, da sempre aquela senssação de quero mais.",
             citacao: "Com açúcar, com afeto, fiz seu doce predileto. Chico Buarque."
 
@@ -18,7 +18,7 @@ function carregaProjetoAutoral() {
 
         bio03: {
             nome: "Crepe",
-            imagem: "https://lh3.googleusercontent.com/UK_hgPdRx4TFutndUhBRn1C1nhtiFmBBpHSUarZsy98zp0FfpnFfh76uqVerdZm7I0dhjnINOSo_PF5PfgEc63Ypua9mbQn7fi7elULXy6KfgTBNFMiFYQxxC7u7rrnhZshpyBXA1Z3WU5Tg_AY4XrwdT13e4wPZf6O4BuPk6xY7YwRD4ozvbgGX-xkitXDHDd6iNrY3QizirYpgrKNqoHy2qSWHOeCNU6IqQXFyDXjRt0m_QTcXi8bXMxyUddmx2T_RQBgEjZYmfkePHCbZzUYRQoyrOA6GOn_VoCfxolXoo9O8BHvPeSh4d7t8xBJnO9jPiUHx4dM6RAy5plgx1Jm5cWMrYVoepsCQJcWKKjzFnFPE5-B3Eu3ozBxsKBPxUhsZd8yZO7ZJuU-6c0cBWazRSWei2aULthb9jnjn2OxEXNL5d0r8Etg6Vta6QV5pTlDJUJm-ayJeE6KvYveIXNg0kGvWdKUcRLnhQJYcqQTGrD5xKgWIpv4adWIV_da7uJbRIu4M9OKwkICDExEwF5RXroGh0IndZfyr52zLANXUhRG99IKHHdknLeDQNgerDgSQVWGPxjzwN2v9XfriBi8Hq27LZPNyGwdmFSQVnFIcNiFOxlDyghy8Gg9DdQU9ExtZahz3IHrg0_qaMznDmzBoJutFhTP0EN3WVggKmkjh2tO9oNDk4Ishtn1S9LevmgFT2Qxj_MeiAaMdvjk0yrPpLHQbeg9WK0P6OpmBXkpSwV1A2uJyrG3wSM4bZ2KMbE_dgRyMH3NBN91Oll4Ynqg86QnI5aWCKdsfVbOzNlmdfDvFEhJnqkh6naKGg5IASJTmbEXL-Q22sf36DC6Q7Lt_7iYWX-OP7MPKlpMSR8tNzZRdil3CHihisqzaw64uL3V_ud9cZFnudbzatd6ISawKv7VqYBVFOi-PsYq9ZH_8sa8HbQ=w720-h883-no?authuser=0",
+            imagem: "https://i.postimg.cc/DyKqKddh/Whats-App-Image-2022-12-16-at-15-26-39.jpg",
             descricao: "crepe de morango com chocolate, olhando para esse prato ele me traz uma saudade de quando minhas meninas era pequeninas, na verdade eu considero como um dos melhores doces da confeitaria.",
             citacao: " Busque sempre olhar o lado doce da vida! Fernanda R. Silva."
 
@@ -33,14 +33,14 @@ function carregaProjetoAutoral() {
 
         bio05: {
             nome: "Codando",
-            imagem: "https://lh3.googleusercontent.com/vu248_yjrvxJeKYQjCgb5JaGQE5ykxkAs-z9wtnczMbgIdcUlGxkCZIbTx87xb89X49fLed-N2xCDwO89q_0BTbdlz6WeR8lDsR6TLLpKBKVN6F-Ty9B00AoSeJqi36MW9a-fPgZZn7P7QNSDQ3yhoTgnN93eIYq0_eqgbVV5VLUhiz75DyCjV-ELdt1qTRTIL5ealNBl35bw6IdS_C3Ma8AfMCBzBi0bmOJFH2iIYmks1K-YlpCvIUqYv7JvYOzk9IHe77mCDTzjWOkzrjSOstJP1RzGhVYO1qLwuC15HfSkX2pYrRrIuphGotgYDTID4Y2C1RaxlCiNXnBSPyMnDrngyEPCaPZqlEofiOhu0VggQzqa0eLadKyXEKGqEZh_aKaiFmVx4IUDs7ApLtQQkeYZtd_BLXemZ1hmBEjvS-vyTqQSq9k_P2-WqKVLfARTwJ_ZNcVc81t7Gz3TWnUZe2qGgWzq-ewKH88zoYkzzNKOyQCqNnFSLTucgKjJAwuji3MVtdRBV_fUPCSEPYFsijWE4yR1Chjg_BLE6uQ_dKBNMTa3esrzYUbD8_gbtsd-yg7Kf_s4IXQW_-00Hqrodj96mlASszQOSEqnzE7CPWveCf7gQtGbiir1imPYfPMcCH4sR26kgzyhPFudzYDShRJc-io12Wo9_LsTC8jygLZUWPV72eocIhddUmsihWTbzhYIUBViWMPtbTEUxLNNELfCfUjvbEm1nGFinECXLBDjsAbU8nl8bY1bCFK_DX96I8FYgYx0nEWBDch4AGCqY4gOvHFpZa7AGrRYLu24-iX_MJ9L5Kc0KLoQ4TzSz4BJMSNMfnsegzo3fhF9NY3dqYYxYiKvBARiRNtNW9oGm78L1G6xXDUqjYjW7FG91IwmXY0dNOsvl5jeXblgyjWd1j8gEpSYTy_GFPimuXDTnjyewD51A=w1258-h943-no?authuser=0",
+            imagem: "https://i.postimg.cc/Hs4bWcBy/Whats-App-Image-2022-12-16-at-15-38-50.jpg",
             descricao: "Em meio a pandemia conheci o mundo dos códigos, com o incentivo das minhas filhas terminei me apaixonando por eles, e descobri que codar é coisa pra mulher sim.",
             citacao: "Quem consegue decifrar os códigos secretos do Destino, é capaz de entender qualquer mensagem enviada por Ele. Fábio Silva."
         },
 
         bio06: {
             nome: "Códigos",
-            imagem: "https://lh3.googleusercontent.com/3yTENazl8-HngHDcmcWUSRCgt8CpmLCGJB5339mbi7FwzI3FR0vG9wLbaaasL-uV4ep6zkJCBMbUuMKYI5kimFw85IevfCKDQORfVfiMy6nmzPCTZQQT0tidCMha1V0g11kVOpup7vVr3dCrLxK6dkfLGe89t7iiEafFXAKG2FDywO7GvU4q5a801rXOt8NBKkaBDWRMRFNaFl9sSQenkciWmaxDIj9cVMp2UlW6WzXJgjU8mmkwzrnf3RI5svmuQIAdT_l-_3fhHEzN1ssIXED0W0DOl_1UB2vmSFSKGbCLdbp6nJM0RVmJ6kdVO3RX4ZAu9tCKtVqJt76QzqMiIKOEaZEEfB-aw7w6My63NzWdy5m2ky2qdSVghUfV1Ve5knUXy3pIHIwyY9chhBqMc99VIqlZDXJ3ADYMKObzQsUcsC36vdOzRIWV8qABBjWNV8V5NxKCTIc2Odc9MRKzpuxihwkt4fu55Vu_eZUUOsKjX8KcI4sNul0NhFkIZuCZbnDbns1WIoYoEZPVRUpO1OpmYsRRmocKwzzt8I1Fq3iN5R8kTp21HjrYdSe0BFuOEkoY0a_shdNXnl9NxDfNV9y0zA_IBQ5eWJ39LS-Wz9LdAKWK-nHPUpIpC7thEvhPVMk9hOZmL1ykxVfGlALSQXYCvNFP8OYAB3LmEObckgXKROpHqYWdiGtmvnMZGie9O5MaZ89K7Mg6ahyAPXXsQGBpOzI3moqVxSAvtuVNIVOVwPyJTgOvenkUPJlaP-6gR8ay2wZ7Ul4WZWWqybYRTQD-nosItfFfKOmzd40BgJvuDJbNzDn9Ib8VW7bvHjZT_MgOfbU3DiK3lvfpumUg4Dfm_vRJUS5H9JaQxVaT40ePYDNx8YFeLb16rnapbYwYhLdWgPkZlP4kTQI4dgTrz3el0lRlEgjZr2O48Z7488-phCiCxQ=w1258-h943-no?authuser=0",
+            imagem: "https://i.postimg.cc/7ZH05HWQ/Whats-App-Image-2022-12-16-at-15-39-28.jpg",
             descricao: "O medo vai sempre existir poque a cada dia surjem novos desafios, novas maneiras de se escrever linhas de códigos, os estudos precisam ser constantes para se alcansar o objetivo.",
             citacao: "A vida é como um código deve-se procurar tudo para decifrar a chave da sua existência. Platino."
         },
